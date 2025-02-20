aac7377c7a9b2018b01e431d239f45f7615d926c
E-paivaa <United_hitz-1@hotmail.com>
2025-02-20 18:00:38 -0500

    Project 14 with Project 15 updates

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const mainRouter = require("./routes/index");
 
@@ -20,6 +22,14 @@ app.use(express.json());
 app.use(cors());
 app.use("/", mainRouter);
 
+// our routes
+app.use(requestLogger);
+app.use(routes);
+
+app.use(errorLogger);
+// celebrate error handler
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ middlewares/validation.js
@@ -0,0 +1,4 @@
+// middleware/validation.js
+
+// const { Joi, celebrate } = require('celebrate');
+// const validator = require('validator');
