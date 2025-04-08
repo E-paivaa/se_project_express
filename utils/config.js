c34d28ef274ff4006e9828b1efa7fb27cd3dbed5
E-paivaa <United_hitz-1@hotmail.com>
2025-04-08 14:01:47 -0400

    Project 15 config fix

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || "super-strong-secret",
-  };
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
+module.exports = { JWT_SECRET };
